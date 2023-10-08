bda26b828dc5d2f92118bc627c7a930b7fa87977
José Diego <josediego_997@hotmail.com>
2023-10-08 10:46:25 +0200

    Init Project

---
+++ src/main/java/com/um/snownote/SnownoteApplication.java
@@ -0,0 +1,13 @@
+package com.um.snownote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnownoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnownoteApplication.class, args);
+	}
+
+}
